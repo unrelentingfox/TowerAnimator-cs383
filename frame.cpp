67362495a7e4d0ec4e21d7c6aae2ae12f5b78b8b
kasanmoor <brandonb.junk@gmail.com>
2016-12-02 01:25:49 -0800

    frames are now able to be selected from the timeline and edited in the animation view.

--- frame.cpp
+++ frame.cpp
@@ -81,6 +81,8 @@ void Frame::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
     mouseClicked = true;
 
+    emit iWasSelected(this);
+
     switch (tool){
     case Globals::ERASE_TOOL:
         //getTowerContents();
--- frame.h
+++ frame.h
@@ -1,16 +1,22 @@
 #ifndef FRAME_H
 #define FRAME_H
+#include <QObject>
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QGraphicsSceneMouseEvent>
+#include "frame.h"
 #include "global.h"
 #include "pixel.h"
 #include <QDebug>
 
 class Frame : public QGraphicsScene
 {
+    Q_OBJECT
+signals:
+    void iWasSelected(Frame*);
 public:
     Frame(int frameNum=0, double frameDuration=0);
+    virtual ~Frame() {};
     void setTool(int t);
     void setRed(int r);
     void setGreen(int g);
